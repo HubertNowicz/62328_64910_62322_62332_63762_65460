facdbf6fa0e7535fc2c1c0da66c89b1c05f3c5a6
HubertNowicz <Nowicz.hubert@wp.pl>
2025-04-29 22:05:12 +0200

    Registerloginsession

---
+++ Organizer_przepisów_kulinarnych/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organizer_przepisów_kulinarnych.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Organizer_przepisów_kulinarnych/Controllers/RegistrationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organizer_przepisów_kulinarnych.BLL.DbContexts;
+using Organizer_przepisów_kulinarnych.BLL.Entities.Enums;
+using Organizer_przepisów_kulinarnych.BLL.Entities;
+
+
+namespace Organizer_przepisów_kulinarnych.Controllers
+{
+    public class RegistrationController : Controller
+    {
+     
+
+        private readonly ApplicationDbContext _context;
+
+        public RegistrationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Organizer_przepisów_kulinarnych.BLL.Entities.User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    Username = model.Username,
+                    FirstName = model.FirstName,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    PasswordHash = model.PasswordHash, // Uwaga: na produkcji trzeba haszować!
+                    UserRole = UserRole.User
+
+                    //UserRole = UserRole.Admin
+                };
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+
+    }
+}
